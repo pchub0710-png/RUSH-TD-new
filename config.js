2a0bc6c841d64238ecc1e3bfffa4256a2c96c1f6
pchub0710-png <pchub0710@gmail.com>
2026-07-30 23:50:27 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    BOT_NAME: process.env.BOT_NAME || "RUSH-TD",
+    PREFIX: process.env.PREFIX || ".",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "947XXXXXXXX", // ඔයාගේ WhatsApp අංකය
+    MODE: process.env.MODE || "public"
+};
